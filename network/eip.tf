57caca57bdc940e21ab09dcea9a1ac915ac069ff
KeitaTsukuba <keitainterncoins@gmail.com>
2022-04-21 02:55:53 +0900

    multi az network

--- network/eip.tf
+++ network/eip.tf
@@ -1,4 +1,17 @@
+########### private ##########
 resource "aws_eip" "nat_gateway" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+########### multi AZ ##########
+# private
+resource "aws_eip" "nat_gateway_0" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_eip" "nat_gateway_1" {
   vpc        = true
   depends_on = [aws_internet_gateway.example]
 }
