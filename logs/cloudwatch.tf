2a084eb991a4bc5aaf7712268b58d2a90627a136
KeitaTsukuba <keitainterncoins@gmail.com>
2022-05-19 02:02:10 +0900

    [WIP]add batch

--- logs/cloudwatch.tf
+++ logs/cloudwatch.tf
@@ -2,3 +2,8 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name              = "/ecs/example"
   retention_in_days = 180
 }
+
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
